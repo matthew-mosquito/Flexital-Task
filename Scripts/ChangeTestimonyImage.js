e246bdcdbba57929c22a949e1a344f023ca40862
matthew-mosquito <matthew@mosquitodigital.co.uk>
2019-06-04 17:10:17 +0100

    Fixing Carousel so images can change the text being shown

---
+++ Scripts/ChangeTestimonyImage.js
@@ -0,0 +1,14 @@
+﻿var flkty = new Flickity('.carousel');
+
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray(buttons);
+
+buttonGroup.addEventListener('click', function (event) {
+    // filter for button clicks
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var index = buttons.indexOf(event.target);
+    flkty.select(index);
+});
